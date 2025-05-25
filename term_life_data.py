35dc86ddfb5df5f03d77875345170d268ebb645b
Pham Hung <phamcongquochung558247@gmail.com>
2025-05-25 21:52:56 +0700

    whole Life Products

--- term_life_data.py
+++ term_life_data.py
@@ -11,7 +11,7 @@ def get_term_data():
         critical_illnesses = ["N","Y"]
 
         # Non-DPI Specific Values
-        non_dpi_coverage_terms = ["1 to 5", "6 to 10", "11 to 15", "16 to 20", "21 to 25", "26 to 30", "31 to 35", "36 to 40", "Above 40"]
+        non_dpi_premium_term = ["2 to 5", "6 to 10", "11 to 15", "16 to 20", "21 to 25", "26 to 30", "31 to 35", "36 to 40", "Above 40"]
         non_dpi_sum_assured_values = ["S$ 50,000", "S$ 100,000", "S$ 200,000", "S$ 300,000", "S$ 400,000", "S$ 500,000", "S$ 750,000", "S$ 1,000,000"]
 
 
@@ -22,13 +22,13 @@ def get_term_data():
             for smoker in smokers:
                 for gender in genders:
                     for critical_illness in critical_illnesses:
-                        for coverage_term in non_dpi_coverage_terms:
+                        for premium_term in non_dpi_premium_term:
                             for sum_assured in non_dpi_sum_assured_values:
                                 term_life_data.append({
                                     "age": age,
                                     "smoker": smoker,
                                     "gender": gender,
-                                    "coverage_term": coverage_term,
+                                    "premium_term": premium_term,
                                     "sum_assured": sum_assured,
                                     "critical_illness": critical_illness,
                                     "category": "non-dpi"
